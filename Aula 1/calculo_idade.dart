b9d71537bd2159b189b5a8852f8ac3d60f32fc81
leandrucarvalho <leandrucs@gmail.com>
2021-04-18 20:26:32 -0300

    primeiro commit

---
+++ Aula 1/calculo_idade.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+main() {
+  // pergunta a idade da pessoas;
+  // se a idade for maior ou igual a 18;
+  // então ele é maior de idade;
+  // se não for
+  // ele não é maior de idade;
+  /* else {
+    print("\n Menor de idade");
+  }*/
+
+  print("===== Digite uma idade =====\n");
+
+  var input = stdin.readLineSync();
+  var idade = int.parse(input!);
+
+  if (idade >= 50) {
+    print("\n Coroa");
+  } else if (idade >= 18) {
+    print("\n Adulto");
+  } else if (idade >= 12) {
+    print("\n Adolescente");
+  } else {
+    print("\n criança");
+  }
+}
